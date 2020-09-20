7d14a0c40d223b41485afeb3b337e955d93f9449
Oradimi <loganargouse@gmail.com>
2020-09-20 20:14:26 +0200

    Various fixes, 7-3 added and raw text patcher
    Added raw text patching (thanks to Tibo for the research and code).
    Covers ship names, equipment names and stats for now.
    7-3 added, "Revamp" menu improved, "Information" menu redone, "Gallery" menu redone, small fixes in supply_main, and item_ishop.
    README updated.

---
+++ EN-patch/kcs2/js/main.js/patcher.js
@@ -0,0 +1,46 @@
+const { readFileSync } = require("fs")
+const { join } = require("path")
+
+const translations = Object.create(null)
+const regexreplacements = []
+
+/**
+ * Grab from KC3 via: ```
+const ships = Object.create(null)
+Object.values(KC3Master.all_ships()).forEach(k => {
+    const name = k.api_name
+    ships[name] = KC3Meta.shipName(name)
+})
+copy(JSON.stringify(ships))
+```
+ */
+for (const file of ["./ignore-ship_names.json", "./ignore-equips.json", "./ignore-ship_types.json", "./ignore-stats.json"])
+    for (const [k,v] of Object.entries(JSON.parse(readFileSync(join(__dirname, file)))))
+        translations[k] = v
+
+for (const file of ["./ignore-stats_list.json"])
+    regexreplacements.push(...Object.entries(JSON.parse(readFileSync(join(__dirname, file)))))
+
+module.exports = (file, contents) => {
+    return contents + `;
+var KCT_TLS = ${JSON.stringify(translations)}
+var KCT_REPLACEMENTS = ${JSON.stringify(regexreplacements)}
+
+Object.defineProperty(PIXI.Text.prototype, "text", {  get() { return this._text; }, set(text) {
+        const replaced = KCT_TLS[text]
+        if (replaced !== undefined)
+            text = replaced
+        else if (text != null) {
+            for (const [from, to] of KCT_REPLACEMENTS)
+                text = text.replace(new RegExp(from, "g"), to)
+        }
+        text = String(text === '' || text === null || text === undefined ? ' ' : text);
+
+        if (this._text === text)
+            return;
+        
+        this._text = text;
+        this.dirty = true;
+}})
+    `
+}
--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-# KanColle English Textures
+# KanColle English Patch
 ![](https://i.imgur.com/kYiiHRo.png)
 
-Update Notice Discord https://discord.gg/krMeMKB
+[Join the Update Notice Discord!](https://discord.gg/krMeMKB)
 
-Translated textures to be used by the assets modifier of KCCacheProxy.
+Translated textures to be used by the assets modifier of [KCCacheProxy](https://github.com/Tibowl/KCCacheProxy/wiki/Installation-and-setup).
+Now also supports raw text patching, such as ship names etc. *(v3.XX)*
+It's enabled by default. To disable raw text patching, follow the instructions below.
 
-Use at your own risk, make sure to clear your cache when updating.
+Use at your own risk, make sure to clear your cache when installing/updating.
 Unlike the POI method, the assets don't break when updates happen in the game.
-No bans as the API is never touched.
+No ban risk as the API is never touched.
 
-Instructions
-Download the master and extract it anywhere, then use KCCP to add the mod
-by selecting the ".mod.json" file inside of the master folder.
+## Instructions
+Download the master and extract it anywhere, then use KCCP (right click in the tray)
+to add the mod by checking "Enable assets modifier", saving, then clicking on "Add a patcher".
+From there, select the "EN-patch.mod.json" file inside of the master folder.
+To disable raw text patching (ship names etc.), open the "EN-patch.mod.json"
+file and replace ("requireScripts": true,) with ("requireScripts": false,).
