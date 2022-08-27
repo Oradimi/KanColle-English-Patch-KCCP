346e71eb8e2da23c6230b095fe7f1da5da33a458
Oradimi <loganargouse@gmail.com>
2022-08-27 23:26:33 +0200

    Update to v3.20.0
    -== Raw Text Patching ==-
    -- Updated --
    - Ship data
    - Equipment data
    
    -- Added --
    - Summer 2022 Event data
    
    -- Fixed --
    - Quest B27 didn't explicitly say to use Haruna as flagship
    - Fleet and LSC unlock text were not being translated due to wrong formatting
    
    -== Texture Patching ==-
    -- Updated --
    - General equipment assets
    
    -- Added --
    - Summer 2022 Event assets
    - Event selectable reward assets
    
    -- Changed --
    - Remade all of the build menu assets ("arsenal" folder)
    - Remade selectable reward assets
    - Remade the Remodel Start button
    - Remade the remainder of the missing organize_filter psds
    - Remade the remainder of the missing common psds
    - Remade the stat sheets in the fleet menu
    - Remade some assets used in the "Info" tab of the fleet menu
    - Updated style of the list legends in most places where it can appear
    - Remastered the enter night battle assets
    
    -== Other Additions ==-
    - Updated the quick updater to v0.5.0!
     -> The browser cache clear preferences will now be saved in a json file!
      -> On second launch onwards, these will be loaded automatically
     -> Some other minor changes to improve the user experience

--- EN-patch-manager/quick_updater.ps1
+++ EN-patch-manager/quick_updater.ps1
@@ -32,20 +32,14 @@ $ENpatchContent = Get-Content -Raw -Path .\EN-patch.mod.json | ConvertFrom-Json;
 $verCur = $ENpatchContent.version;
 
 # Old version names support;
-If($verCur.length -lt 6)
-{
-	If($verCur.contains("a"))
-	{
+If ($verCur.length -lt 6) {
+	If ($verCur.contains("a")) {
 		$verCur = $verCur.substring(0,4);
 		$verCur += ".1"
-	}
-	ElseIf($verCur.contains("b"))
-	{
+	} ElseIf ($verCur.contains("b")) {
 		$verCur = $verCur.substring(0,4);
 		$verCur += ".2"
-	}
-	Else
-	{
+	} Else {
 		$verCur += ".0"
 	}
 };
@@ -58,19 +52,15 @@ $delURI = New-Object System.Collections.ArrayList($null);
 $addURI = New-Object System.Collections.ArrayList($null);
 $i = 0;
 $j = 0;
-ForEach($ver in $versionContent.version)
-{
-	If($verNewFlag)
-	{
+ForEach ($ver in $versionContent.version) {
+	If ($verNewFlag) {
 		$verNew = $versionContent[$i];
 		$delURI += ,@($verNew.del);
         $delURI[$j] = [System.Collections.ArrayList]$delURI[$j];
 		$addURI += ,@($verNew.add);
         $addURI[$j] = [System.Collections.ArrayList]$addURI[$j];
         $j += 1
-	}
-	ElseIf($ver -eq $verCur)
-	{
+	} ElseIf ($ver -eq $verCur) {
 		$verNewFlag = $true
 	}
 	$i += 1
@@ -78,8 +68,7 @@ ForEach($ver in $versionContent.version)
 $verSkip = $j - 1;
 
 # If current version is the same as the latest;
-If($j -eq 0)
-{
+If ($j -eq 0) {
 	Write-Host "No new version available, or invalid current version." -ForegroundColor Yellow;
 	Write-Host "If you are still using v1 or v2 of the English Patch," -ForegroundColor Yellow;
 	Write-Host "please get the latest version from GitHub." -ForegroundColor Yellow;
@@ -89,22 +78,14 @@ If($j -eq 0)
 	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 	Write-Host "";
 	Write-Host "";
-	Write-Host "";
-	Write-Host "";
-	Write-Host "";
-	Write-Host "";
 	Exit
 };
 
 # Removes any duplicate mention of added/modified files;
-For($i = $verSkip; $i -ge 1; $i--)
-{
-	For($j = $i - 1; $j -ge 0; $j--)
-	{
-		ForEach($k in $addURI[$i])
-		{
-			If($addURI[$j] -contains $k)
-			{
+For ($i = $verSkip; $i -ge 1; $i--) {
+	For ($j = $i - 1; $j -ge 0; $j--) {
+		ForEach ($k in $addURI[$i]) {
+			If ($addURI[$j] -contains $k) {
 				$addURI[$j].Remove($k)
 			}
 		}
@@ -112,39 +93,23 @@ For($i = $verSkip; $i -ge 1; $i--)
 };
 
 # Deletes then downloads each mentionned file, version after version;
-For($i = 0; $i -le $verSkip; $i++)
-{
-    ForEach($uri in $delURI[$i])
-    {
-		If (Test-Path -Path ($pwd + $uri))
-		{
+For ($i = 0; $i -le $verSkip; $i++) {
+    ForEach ($uri in $delURI[$i]) {
+		If (Test-Path -Path ($pwd + $uri)) {
 			Write-Host "Deleting" $uri"...";
 			Remove-Item -Path ($pwd + $uri) -Recurse -Force
 		}
-    }
-    ForEach($uri in $addURI[$i])
-    {
-		Try
-		{
+    } ForEach ($uri in $addURI[$i]) {
+		Try {
 			Write-Host "Downloading" $uri"...";
 			Invoke-WebRequest ($gitPath + $uri) -O (New-Item -Path ($pwd + $uri) -Force)
-		}
-		Catch [System.Net.WebException]
-		{
+		} Catch [System.Net.WebException] {
 			Write-Host "File deleted from server, skipping" $uri
 		}
     }
 };
 
 Write-Host "";
-Write-Host "Done updating! Please clear your cache to finish the update process." -ForegroundColor Green;
-Write-Host "";
-Write-Host "-> Close this window to not proceed to clear your cache.";
-Write-Host "-> Press any key to proceed to clear your cache...";
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-Write-Host "";
-Write-Host "";
-Write-Host "";
-Write-Host "";
+Write-Host "Done updating! Proceeding to cache clear..." -ForegroundColor Green;
 Write-Host "";
 Write-Host "";
--- _clear_browser_cache.bat
+++ _clear_browser_cache.bat
@@ -1,5 +1,5 @@
 @echo off
  
-powershell.exe -ExecutionPolicy Unrestricted -Command ". 'EN-patch-manager\clear_browser_cache.ps1'"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ". 'EN-patch-manager\clear_browser_cache.ps1'"
  
 TIMEOUT /T 60
